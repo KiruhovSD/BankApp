be496d2fccb07b32d599f5b9d73c866916e636bf
KiruhovSD <sergeykiruhov@gmail.com>
2020-11-26 23:45:26 +0300

    BankApp

---
+++ BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by spbtrak on 26.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
